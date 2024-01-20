fb8265440325f1895b4948e155e4924ec8aaece4
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-01-20 09:33:16 +0600

    project setup succesfully

---
+++ app/popular/[id]/page.jsx
@@ -0,0 +1,9 @@
+const PopularDetails = () => {
+  return (
+    <div>
+      <h1>Popular Details</h1>
+    </div>
+  )
+}
+
+export default PopularDetails
---
+++ app/popular/page.jsx
@@ -0,0 +1,9 @@
+const Popular = () => {
+  return (
+    <div>
+      <h1>This is popular page</h1>
+    </div>
+  )
+}
+
+export default Popular
